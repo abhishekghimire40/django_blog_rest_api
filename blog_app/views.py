f64cb04330e90f157b6c8cad9e8167c8b2e2bafe
abhishekghimire40 <abhishekghimire40@gmail.com>
2023-07-30 22:02:45 +0545

    created models and two view one list all blogs and another returns detail of a blog as json reponse

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Blog
 
 
-# Create your views here.
+# returns list of blogs
 def blog_list(request):
-    message = {
-        "body": "Hello world",
-    }
-    return JsonResponse(message)
+    blogs = Blog.objects.all()
+    # data = {"blogs": list(blogs.values())}
+    data = {"blogs": list(blogs.values())}
+    return JsonResponse(data)
+
+
+# returns detail of a blog
+def blog_detail(request, pk):
+    try:
+        blog = Blog.objects.get(id=pk)
+        data = {
+            "name": blog.name,
+            "description": blog.description,
+            "slug": blog.slug,
+        }
+        return JsonResponse(data)
+    except ObjectDoesNotExist:
+        return JsonResponse(
+            {
+                "error": "blog with provided id doesn't exist",
+            },
+            status=404,
+        )
