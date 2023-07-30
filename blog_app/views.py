f1bd9b18482ab5be0957a1d3bfffab71560e70a2
abhishekghimire40 <abhishekghimire40@gmail.com>
2023-07-30 20:18:12 +0545

    created simple api endpoint that returns hello world json response

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+def blog_list(request):
+    message = {
+        "body": "Hello world",
+    }
+    return JsonResponse(message)
